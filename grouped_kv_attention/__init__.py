d30050de676b979d202ee2d9dbbfaf1004f6236a
Kevin Kuo <kevin0978159517@gmail.com>
2025-04-09 14:33:46 +0800

    add mistral & gemm2 patch

--- grouped_kv_attention/__init__.py
+++ grouped_kv_attention/__init__.py
@@ -1,7 +1,11 @@
 from grouped_kv_attention.patch import (
     patch_llama_attention,
+    patch_mistral_attention,
+    patch_gemma2_attention
 )
 
 __all__ = [
-    "patch_llama_attention"
+    "patch_llama_attention",
+    "patch_mistral_attention",
+    "patch_gemma2_attention"
 ]

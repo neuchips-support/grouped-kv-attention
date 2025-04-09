f580da60bba5992776aa583140b18490ee2ba0b3
Kevin Kuo <kevin0978159517@gmail.com>
2025-04-09 15:24:03 +0800

    Update patch.py

--- grouped_kv_attention/patch.py
+++ grouped_kv_attention/patch.py
@@ -3,7 +3,7 @@ from grouped_kv_attention.grouped_attention import GroupedKVAttention
 def patch_llama_attention(config=None):
     from transformers.models.llama import modeling_llama
 
-    modeling_llama.eager_attention_forward = GroupedKVAttention(config).grouped_kv_attention_forward()
+    modeling_llama.eager_attention_forward = GroupedKVAttention(config).grouped_kv_attention_forward
 
     original_init = modeling_llama.LlamaAttention.__init__
     
@@ -16,7 +16,7 @@ def patch_llama_attention(config=None):
 def patch_mistral_attention(config=None):
     from transformers.models.mistral import modeling_mistral
 
-    modeling_mistral.eager_attention_forward = GroupedKVAttention(config).grouped_kv_attention_forward()
+    modeling_mistral.eager_attention_forward = GroupedKVAttention(config).grouped_kv_attention_forward
 
     original_init = modeling_mistral.MistralAttention.__init__
     
@@ -29,7 +29,7 @@ def patch_mistral_attention(config=None):
 def patch_gemma2_attention(config=None):
     from transformers.models.gemma2 import modeling_gemma2
 
-    modeling_gemma2.eager_attention_forward = GroupedKVAttention(config).grouped_kv_attention_forward()
+    modeling_gemma2.eager_attention_forward = GroupedKVAttention(config).grouped_kv_attention_forward
 
     original_init = modeling_gemma2.Gemma2Attention.__init__
     
